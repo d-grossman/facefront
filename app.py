8e9ad5d87c5ab5dd055623a0275589ea2091a39c
dgrossman <dgrossman@ForbinProject.local>
2017-08-12 13:10:47 -0700

    py2 -> py3

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Resource, Api
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+app = Flask(__name__)
+api = Api(app)
+
+class working(Resource):
+    def get(self):
+        return{'working':'yes'}
+
+api.add_resource(working,'/')
 
 if __name__ == '__main__':
+    #TODO load the pickl file
     app.run(debug=True,host='0.0.0.0')
