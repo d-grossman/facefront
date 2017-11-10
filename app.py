8ef42214e85d775bab9f11320dbd851eccd2c81e
dgrossman <dgrossman@xyxxl.com>
2017-11-09 19:24:04 -0800

    updated endpoints

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import os
 import pickle
 import sys
 import time
+import os
 from collections import defaultdict
+from os import listdir
+from os.path import isfile, join
+import json
 
 import cv2
 import numpy as np
@@ -27,9 +31,24 @@ face_search_vectors = None
 face_group_search = None
 hash2file = None
 
-JITTERS=0
+JITTERS = 0
 
 parser = reqparse.RequestParser()
+
+
+def get_vid_length(uri):
+    video_file = cv2.VideoCapture(uri)
+    video_length = int(video_file.get(cv2.CAP_PROP_FRAME_COUNT))
+    video_file.release()
+    return video_length
+
+
+def get_vid_frame(uri, frame_number):
+    video_file = cv2.VideoCapture(uri)
+    video_file.set(1, frame_number)
+    keep_going, img = video_file.read()
+    video_file.relase()
+    return keep_going, img
 
 
 class return_frame(Resource):
@@ -57,16 +76,13 @@ class return_frame(Resource):
             abort(404, message='frame_number {0} must be >0'.format(
                 frame_number))
 
-        video_file = cv2.VideoCapture(uri)
-        video_length = int(video_file.get(cv2.CAP_PROP_FRAME_COUNT))
+        video_length = get_vid_length(uri)
 
         if frame_number > video_length:
             abort(404, message='{0} > max length of vid {1}'.format(
                 frame_number, video_length))
 
-        video_file.set(1, frame_number)
-
-        keep_going, img = video_file.read()
+        keep_going, img = get_vid_frame(uri, frame_number)
 
         if keep_going:
 
@@ -75,21 +91,64 @@ class return_frame(Resource):
             meta['frame_number'] = frame_number
             ret_val['meta'] = meta
             ret_val['frame'] = write_frame(file_hash, frame_number, img)
-            # video_file.close()
             return ret_val
 
         else:
-            # video_file.close()
             abort(404, message='frame decode error')
 
 
-def handle_post_file():
+def process_additional_vectors(additional_vectors):
+    if additional_vectors is None or len(additional_vectors) == 0:
+        return []
+    retval = []
+    onlyfiles = [f for f in listdir(
+        app.config['UPLOAD_DIR']) if isfile(join(mypath, f))]
+    hash2name = {}
+    for file in onlyfiles:
+        name = file.split('.')[0]
+        hash2name['name'] = file
+
+    for a_vector in additional_vectors:
+        filename = hash2name[a_vector]
+
+        face_image = cv2.imread(os.path.join(
+                                app.config['UPLOAD_FOLDER'], filename))
+
+        # get location of face so I can return it to gui.
+        list_face_locs = face.face_locations(face_image, 2)
+        enc = None
+
+        (top, right, bottom, left) = list_face_locs[0]
+        face_array = face_image[top:bottom, left:right]
+
+        if app.config['normalize']:
+            # normalize
+            list_face_encodings = normalize_faces(
+                face_image, list_face_locs, 2)
+            enc = list_face_encodings[0][0]
+        else:
+            #not normalize
+            enc = face.face_encodings(face_image, list_face_locs)[0]
+
+        loc = list_face_locs[0]
+
+        print('enc_len:', len(enc))
+        sys.stdout.flush()
+
+        # make a reference to the vector as a loose hash to the file
+        #h = vec2hash(enc)
+        h = pic2hash(face_array)
+        temp = (loc, enc, h)
+        retval.append(temp)
+
+
+def handle_post_file(additional_vectors):
     # get the image if it exists
     retval = []
 
-    # print('******************************')
-    #print('request.files:', request.files)
-    #print('request.files.keys:', list(request.files.keys()))
+    print('******************************')
+    print('request.files:', request.files)
+    print('request.files.keys:', list(request.files.keys()))
     sys.stdout.flush()
 
     for file_key in request.files.keys():
@@ -141,6 +200,8 @@ def handle_post_file():
         h = pic2hash(face_array)
         temp = (loc, enc, h)
         retval.append(temp)
+    more_results = process_additional_vectors(additional_vectors)
+    retval = retval + more_results
     return retval
 
 
@@ -163,8 +224,70 @@ class return_feeds(Resource):
             val['file_content_hash'] = hash2file[h]['Hash']
             val['location'] = hash2file[h]['Location']
             val['uri'] = '/static/' + val['name']
+            val['frames'] = hash2file[h]['frames']
             results.append(val)
         ret_val['results'] = results
+        return ret_val
+
+    def post(self):
+        ret_val = {}
+        additional_vectors = None
+
+        dist_name = 'threshold'
+
+        if dist_name not in request.form:
+            abort(404, message='threshold not specified')
+
+        distance = float(request.form[dist_name])
+
+        if distance > 1.0 or distance < 0:
+            abort(
+                404,
+                message='threshold {0} must be between [0,1]'.format(distance))
+        sys.stdout.flush()
+
+        loc_enc_h = handle_post_file(additional_vectors)
+        if len(loc_enc_h) != 1:
+            abort(404, message='expected exactly 1 faces, found {0}'.format(
+                len(loc_enc_h)))
+
+        video_hits = defaultdict(int)
+
+        query_src_loc, query_src_enc, query_src_h = loc_enc_h[0]
+
+        for key in face_pickle:
+            s_entity = face_pickle[key]
+            s_vec = np.array(s_entity['face_vec'])
+            s_videos = s_entity['videos']
+
+            left = query_src_enc
+            right = s_vec
+
+            vector_distance = face.face_distance([left], right)[0]
+            if vector_distance < distance:
+                for vid_key in s_videos:
+                    video_hits[vid_key] += len(s_videos[vid_key])
+
+        ret_val['meta'] = {}
+        ret_val['meta']['distance'] = distance
+        ret_val['meta']['vector_set'] = {}
+        ret_val['meta']['vector_set']['count'] = len(loc_enc_h)
+        ret_val['meta']['vector_set']['vectors'] = []
+        for loc, enc, h in loc_enc_h:
+            d = {}
+            d['hash'] = h
+            d['face_pic_hash'] = h
+            d['face_coordinates'] = list(loc)
+            d['vector'] = list(enc)
+            ret_val['meta']['vector_set']['vectors'].append(d)
+
+        ret_val['results'] = []
+        for vh in video_hits:
+            d = {}
+            d['video'] = vh
+            d['count'] = video_hits[vh]
+            ret_val['results'].append(d)
+
         return ret_val
 
 
@@ -174,12 +297,88 @@ class working(Resource):
         return{'working': 'yes'}
 
 
+class return_framelocation(Resource):
+
+    def post(self):
+
+        ret_val = {}
+
+        dist_name = 'threshold'
+
+        if dist_name not in request.form:
+            abort(404, message='threshold not specified')
+
+        distance = float(request.form[dist_name])
+
+        if distance > 1.0 or distance < 0:
+            abort(
+                404,
+                message='threshold {0} must be between [0,1]'.format(distance))
+
+        if 'vector' not in request.form:
+            abort(404, message='vector not specified')
+
+        if 'video' not in request.form:
+            abort(404, message='video not specified')
+
+        search_vector = request.form['vector']
+        search_vector = json.loads(search_vector)
+
+        video = request.form['video']
+
+        ret_val['meta'] = {}
+        ret_val['meta']['vector_set'] = {}
+        ret_val['meta']['vector_set']['count'] = 1
+        ret_val['meta']['distance'] = distance
+        ret_val['meta']['video'] = video
+        ret_val['meta']['vector_set']['vectors'] = []
+
+        d = {}
+        d['hash'] = None
+        d['face_pic_hash'] = None
+        d['face_coordinates'] = None
+        d['vector'] = search_vector
+
+        ret_val['meta']['vector_set']['vectors'].append(d)
+        ret_val['results'] = {}
+
+
+        video_hits = {}
+
+        for key in face_pickle:
+            s_entity = face_pickle[key]
+            s_pic = s_entity['face_pic']
+            s_vec = np.array(s_entity['face_vec'])
+            s_videos = s_entity['videos']
+
+            left = search_vector
+            right = s_vec
+
+            vector_distance = face.face_distance([left], right)[0]
+            if vector_distance < distance:
+                if video in s_videos:
+                    d = {}
+                    d['face_pic'] = 'comming soon'
+                    d['frames'] = []
+
+                    for frame in s_videos[video]:
+                        d['frames'].append(frame)
+
+                    video_hits[key] = d
+
+        ret_val['results'] = video_hits
+        ret_val['meta']['num_results'] = len(video_hits.keys())
+
+        return ret_val
+
+
 class make_results_comparisons(Resource):
 
     def post(self):
         ret_val = {}
+        additional_vectors = None
 
-        loc_enc_h = handle_post_file()
+        loc_enc_h = handle_post_file(additional_vectors)
         if len(loc_enc_h) != 2:
             abort(404, message='expected exactly 2 faces, found {0}'.format(
                 len(loc_enc_h)))
@@ -212,7 +411,8 @@ class make_results_matches(Resource):
         meta = {}
 
         dist_name = 'threshold'
-
+        vector_name = 'vectors'
+        additional_vectors = []
         #args = parser.parse_args()
         #print('args:', args)
         #print('request:', request)
@@ -221,6 +421,10 @@ class make_results_matches(Resource):
 
         if dist_name not in request.form:
             abort(404, message='threshold not specified')
+
+        if vector_name in request.form:
+            for vector in request.form[vector_name]:
+                additional_vectors.append(vector)
 
         distance = float(request.form[dist_name])
 
@@ -234,7 +438,7 @@ class make_results_matches(Resource):
         query['threshold'] = distance
         meta['query'] = query
 
-        loc_enc_h = handle_post_file()
+        loc_enc_h = handle_post_file(additional_vectors)
         meta['vector_set'] = self.make_vector_set(loc_enc_h)
 
         ret_val['meta'] = meta
@@ -367,6 +571,8 @@ api.add_resource(working, app.config['V1.0'] + '/working')
 api.add_resource(make_results_matches, app.config['V1.0'] + '/results/matches')
 api.add_resource(make_results_comparisons,
                  app.config['V1.0'] + '/results/comparisons')
+api.add_resource(return_framelocation, app.config['V1.0'] + '/framelocation')
+
 
 if __name__ == '__main__':
     # load the pickl file
--- helpers.py
+++ helpers.py
@@ -5,6 +5,7 @@ import sys
 from collections import defaultdict
 
 import numpy as np
+import cv2
 from PIL import Image
 
 
@@ -71,10 +72,16 @@ def hash_files(location):
         ext = f.split('.')[-1]
         link_src = f
         link_dest = os.path.join('/app/static', name)
-        os.symlink(link_src, link_dest)
+
+        if not os.path.exists(link_dest):
+            os.symlink(link_src, link_dest)
+
         just_hash_name = link_dest.split('_')[-1]
-        just_hash_name = os.path.join('/app/static',just_hash_name)
-        os.symlink(link_src, just_hash_name)
+        just_hash_name = os.path.join('/app/static', just_hash_name)
+
+        if not os.path.exists(just_hash_name):
+            os.symlink(link_src, just_hash_name)
+
         sys.stdout.flush()
         if ext in ['avi', 'mov', 'mp4']:
             file_hash = file_digest(f)
@@ -82,6 +89,9 @@ def hash_files(location):
             ret_val[file_hash]['Hash'] = file_hash
             ret_val[file_hash]['file_content_hash'] = file_hash
             ret_val[file_hash]['Name'] = name
+            video_file = cv2.VideoCapture(f)
+            video_length = int(video_file.get(cv2.CAP_PROP_FRAME_COUNT))
+            ret_val[file_hash]['frames'] = video_length
             print('found: {0}:{1}'.format(file_hash, f.split('/')[-1]))
             sys.stdout.flush()
     return ret_val
